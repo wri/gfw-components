8b05791c7a981e0a329d558a05047f7571ec0d7d
Ed Brett <e.j.a.brett@gmail.com>
2020-04-01 13:44:51 +0200

    expose icon components to dist

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -4,6 +4,15 @@ import ArrowIcon from 'assets/icons/arrow-down.svg';
 import CloseIcon from 'assets/icons/close.svg';
 import MenuIcon from 'assets/icons/menu.svg';
 import MoreIcon from 'assets/icons/more.svg';
+import SearchIcon from 'assets/icons/search.svg';
+import MyGFWIcon from 'assets/icons/mygfw.svg';
+
+import DeveloperIcon from 'assets/icons/developer.svg';
+import BlogIcon from 'assets/icons/blog.svg';
+import ForumIcon from 'assets/icons/forum.svg';
+import HowToIcon from 'assets/icons/howto.svg';
+import OpenDataIcon from 'assets/icons/open-data.svg';
+import SGFIcon from 'assets/icons/sgf.svg';
 
 import './styles.scss';
 
@@ -11,10 +20,22 @@ class Icons extends Component {
   render() {
     return (
       <div className="c-icons">
-        <ArrowIcon />
-        <CloseIcon />
-        <MenuIcon />
-        <MoreIcon />
+        <div className="row">
+          <ArrowIcon />
+          <CloseIcon />
+          <MenuIcon />
+          <MoreIcon />
+          <MyGFWIcon />
+          <SearchIcon />
+        </div>
+        <div className="row">
+          <DeveloperIcon />
+          <BlogIcon />
+          <ForumIcon />
+          <HowToIcon />
+          <OpenDataIcon />
+          <SGFIcon />
+        </div>
       </div>
     );
   }
