49bab712c9e2c36cce6e5915e85ae5dc277c4c94
Ed Brett <e.j.a.brett@gmail.com>
2020-04-06 12:15:42 +0200

    remove static rendering function from index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Header from './components/header';
-
-export const showHeader = element => {
-  const el = document.getElementById('headerGfw');
-  if (!el) {
-    throw new Error("element #headerGfw doesn't exist");
-  }
-  ReactDOM.render(<Header />, element);
-};
-
-if (
-  !!document.readyState &&
-    (document.readyState === 'complete' ||
-      document.readyState === 'interactive')
-) {
-  showHeader();
-} else {
-  document.addEventListener('DOMContentLoaded', showHeader);
-}
-
 // Icons
 export { default as ArrowIcon } from './assets/icons/arrow-down.svg';
 export { default as CloseIcon } from './assets/icons/close.svg';
