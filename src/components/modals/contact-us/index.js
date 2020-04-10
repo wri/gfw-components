af8a23aff786e558de49467f9b11b870abca3521
Ed Brett <e.j.a.brett@gmail.com>
2020-04-10 10:59:09 +0200

    fix contact us modal import

--- src/components/modals/contact-us/index.js
+++ src/components/modals/contact-us/index.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-import ContactForm from 'components/forms/contact';
+import ContactForm from 'components/forms/contact-us';
 import Modal from 'components/modals/modal';
 
 import './styles.scss';
