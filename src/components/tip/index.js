15570540bdedbd5f838729a6005e4dc27af422e3
Ed Brett <e.j.a.brett@gmail.com>
2020-05-09 20:49:22 +0200

    tip component

--- src/components/tip/index.js
+++ src/components/tip/index.js
@@ -1,19 +1,18 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import cx from 'classnames';
 
-import './tip.scss';
+import { TipWrapper } from './styles';
 
 class Tip extends PureComponent {
   render() {
     const { text, className } = this.props;
-    return <div className={cx('c-tip', className)}>{text}</div>;
+    return <TipWrapper className={className}>{text}</TipWrapper>;
   }
 }
 
 Tip.propTypes = {
   text: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 export default Tip;
