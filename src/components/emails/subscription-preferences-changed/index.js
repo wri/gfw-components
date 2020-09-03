547242608a626d325f3303b9eff963dba140e377
Ed Brett <e.j.a.brett@gmail.com>
2020-09-03 17:57:45 +0100

    subscription preferences changed email template

---
+++ src/components/emails/subscription-preferences-changed/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Template from './subscription-preferences-changed.html';
+
+const SubscriptionPreferencesChangedTemplate = () => (
+  <div dangerouslySetInnerHTML={{ __html: Template }} />
+);
+
+export default SubscriptionPreferencesChangedTemplate;
