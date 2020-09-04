fcaaf43352eb725c081a1334a95b5eb4f11539d3
Ed Brett <e.j.a.brett@gmail.com>
2020-09-04 16:55:22 +0100

    add french for contact us email

--- src/components/emails/contact-us-confirmation/index.js
+++ src/components/emails/contact-us-confirmation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Template from './contact-us-confirmation.html';
+import Template from './contact-us-confirmation-fr.html';
 
 const ContactUsConfirmationTemplate = () => (
   <div dangerouslySetInnerHTML={{ __html: Template }} />
