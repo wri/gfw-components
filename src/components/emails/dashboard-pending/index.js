2763de2f7f5517c19c76ca7ea776a723ced72802
Ed Brett <e.j.a.brett@gmail.com>
2020-09-04 14:54:20 +0100

    docs for dashboard pending email

--- src/components/emails/dashboard-pending/index.js
+++ src/components/emails/dashboard-pending/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { globalStringReplace } from 'utils/strings';
 
 import Template from './dashboard-pending.html';
 
+const demoVars = {
+  name: 'area of interest',
+  tags: 'tag1, tag2',
+  subscriptions_url: 'http://test.com',
+  dashboard_link: 'http://test.com',
+  map_link: 'http://test.com',
+};
+
 const DashboardPendingTemplate = () => (
-  <div dangerouslySetInnerHTML={{ __html: Template }} />
+  <div
+    dangerouslySetInnerHTML={{
+      __html: globalStringReplace(Template, demoVars),
+    }}
+  />
 );
 
 export default DashboardPendingTemplate;
