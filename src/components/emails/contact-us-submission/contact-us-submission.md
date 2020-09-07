eafc0a07e17d31b4317d9084359f367c3d353aab
Ed Brett <e.j.a.brett@gmail.com>
2020-09-07 12:20:22 +0100

    add contact us submission template

---
+++ src/components/emails/contact-us-submission/contact-us-submission.md
@@ -0,0 +1,5 @@
+Email sent to GFW when a submission is made through the contact us form.
+
+```jsx
+<ContactUsSubmissionTemplate />
+```
---
+++ src/components/emails/contact-us-submission/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { globalStringReplace } from 'utils/strings';
+
+import Template from './contact-us-submission-en.html';
+
+const demoVars = {
+  user_email: 'david@test.com',
+  topic: 'topic',
+  tool: 'flagship',
+  message: 'Hello I am asking about something.',
+  opt_in: 'yes',
+};
+
+const ContactUsSubmissionTemplate = () => (
+  <div
+    dangerouslySetInnerHTML={{
+      __html: globalStringReplace(Template, demoVars),
+    }}
+  />
+);
+
+export default ContactUsSubmissionTemplate;
