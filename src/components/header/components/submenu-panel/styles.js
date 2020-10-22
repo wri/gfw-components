e872aa07f50769dc3b67bd1a539e9f2c63018a9d
Tomas Eriksson <tomasmoosee@gmail.com>
2020-10-22 14:44:57 +0200

    set max width to 800 for submenu(fullscreen) same as before

--- src/components/header/components/submenu-panel/styles.js
+++ src/components/header/components/submenu-panel/styles.js
@@ -40,7 +40,7 @@ export const SubmenuWrapper = styled.div`
       `
       width: 100%;
       height: 100%;
-      max-width: ${theme.siteDefaultWidth};
+      max-width: 800px;
       margin: 0 auto;
     `}
     background-color: #fff;
